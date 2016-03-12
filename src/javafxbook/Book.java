0d84a0f89f11833c7ffb1ef68baaed4fc55cf39c
stefan <stefan@silverdrop.trillium.lan>
2016-03-12 09:23:45 +0000

    1.1 - Book class added

---
+++ src/javafxbook/Book.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxbook;
+
+/**
+ *
+ * @author stefan
+ */
+public class Book {
+    
+    private String title;
+    
+    public Book(String t){
+        title = t ;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    
+    
+}
